a6df0559e18ee8bb0fd649bf5382b0ecffb57c73
Jan Kiene <jankiene@onlinehome.de>
2018-04-16 13:43:49 +0200

    move functionality into cfg object

--- miditri.py
+++ miditri.py
@@ -3,25 +3,28 @@
 
 import click
 import mido
+from os.path import basename
 
 
-class Config(object):
+class Miditri(object):
 
     def __init__(self, infile, outfile):
         self.infile = infile
         self.midifile = mido.MidiFile(infile)
-        self.outfile = outfile
+        if outfile is None:
+            self.outfile = basename(infile).split('.mid')[0]
+        else:
+            self.outfile = outfile
+
+    def write_results(self):
+        with open(self.outfile, 'w') as f:
+            f.write('hello')
 
 @click.command()
 @click.argument('infile', type=click.Path(exists=True), required=True)
-@click.option('--outfile', type=click.File('w'), default=None,
+@click.option('--outfile', type=str, default=None,
               help='Path to write results to (in .csv format)')
 def cli(infile, outfile):
     """Extract various melody-related information from a given MIDI file (INFILE)."""
-    cfg = Config(infile, outfile)
-    click.echo(cfg.infile)
-    click.echo(cfg.outfile)
-
-def write_results(outfile):
-    if outfile is None:
-        pass
+    miditri = Miditri(infile, outfile)
+    miditri.write_results()
