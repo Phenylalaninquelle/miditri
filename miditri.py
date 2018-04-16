bbfc0583c3f787ce37113312aa57fb60d3f98a46
Jan Kiene <jankiene@onlinehome.de>
2018-04-16 13:26:51 +0200

    add config object and help text

--- miditri.py
+++ miditri.py
@@ -4,10 +4,24 @@
 import click
 import mido
 
+
+class Config(object):
+
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.midifile = mido.MidiFile(infile)
+        self.outfile = outfile
+
 @click.command()
 @click.argument('infile', type=click.Path(exists=True), required=True)
-@click.option('--outfile', type=click.File('w'))
+@click.option('--outfile', type=click.File('w'), default=None,
+              help='Path to write results to (in .csv format)')
 def cli(infile, outfile):
-    """Extract various melody-related information from a given MIDI file."""
-    mf = mido.MidiFile(infile)
-    click.echo('done')
+    """Extract various melody-related information from a given MIDI file (INFILE)."""
+    cfg = Config(infile, outfile)
+    click.echo(cfg.infile)
+    click.echo(cfg.outfile)
+
+def write_results(outfile):
+    if outfile is None:
+        pass
